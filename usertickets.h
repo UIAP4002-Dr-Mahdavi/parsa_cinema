8537e9e04bb7dd40566d1ebd787e12fb33d54942
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-06-25 13:35:56 +0430

    tickets class

---
+++ usertickets.h
@@ -0,0 +1,14 @@
+#ifndef USERTICKETS_H
+#define USERTICKETS_H
+
+
+class userTickets
+{
+public:
+    userTickets();
+    char password[30];
+    char film_name[30];
+    int numberOfTickets;
+};
+
+#endif // USERTICKETS_H
